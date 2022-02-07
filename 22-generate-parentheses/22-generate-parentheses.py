83e0d0e717a6aa6c1d4c78a7609ca3f8df458029
Bhavesh S Singh <66252720+BhaveshSSingh@users.noreply.github.com>
2022-02-07 19:55:46 +0530

    Time: 20 ms (88.76%), Space: 13.7 MB (66.02%) - LeetHub

--- 22-generate-parentheses/22-generate-parentheses.py
+++ 22-generate-parentheses/22-generate-parentheses.py
@@ -3,28 +3,25 @@ class Solution(object):
         """
         :type n: int
         :rtype: List[str]
-        
-        1. Only add open paranthasis if open < n
-        2. Only add close paranthasis if close < open
-        3. Valid if open == close == n 
         """
-        stack = []
-        res= []
+        stack =[]
+        res=[]
+        
         
         def backtrack(open,closed):
             if open == closed == n:
-                return res.append("".join(stack))
+                res.append("".join(stack))
+            
+            if open > closed:
+                stack.append(")")
+                backtrack(open,closed+1)
+                stack.pop()
             
             if open < n:
                 stack.append("(")
-                backtrack(open + 1,closed)
+                backtrack(open+1,closed)
                 stack.pop()
-            
-            if closed < open : 
-                stack.append(")")
-                backtrack(open,closed +1)
-                stack.pop()
-            
+        
         backtrack(0,0)
         return res
-            
+        
