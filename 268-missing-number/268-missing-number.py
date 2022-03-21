3a7ab76ea29b6f82ab524273662ee775048d6aca
Bhavesh S Singh <66252720+BhaveshSSingh@users.noreply.github.com>
2022-03-21 20:27:44 +0530

    Time: 151 ms (78.16%), Space: 15.2 MB (83.77%) - LeetHub

--- 268-missing-number/268-missing-number.py
+++ 268-missing-number/268-missing-number.py
@@ -1,20 +1,4 @@
-class Solution(object):
-    def missingNumber(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        # ans =  len(nums) - 1
-        # print len(nums)
-        # return ans
-        
-        # res = len(nums)
-        # # print(res)
-        # for i in range(len(nums)):
-        #     res += (i - nums[i])
-        #     print res
-        # return res
-        
-        ans =sum(range(len(nums)+1)) - sum(nums) 
-        # print (range(len(nums)+1))
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        ans = sum(range(len(nums)+1)) - sum(nums)
         return ans
