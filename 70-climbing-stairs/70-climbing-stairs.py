b61932f60ab4029368132ec7fef8d2a9caae757b
Bhavesh S Singh <66252720+BhaveshSSingh@users.noreply.github.com>
2022-03-24 16:06:31 +0530

    Time: 28 ms (94.25%), Space: 14 MB (9.85%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.py
+++ 70-climbing-stairs/70-climbing-stairs.py
@@ -1,14 +1,8 @@
-class Solution(object):
-    def climbStairs(self, n):
-        """
-        :type n: int
-        :rtype: int
-        """
+class Solution:
+    def climbStairs(self, n: int) -> int:
         one, two = 1,1
-        
         for i in range(n-1):
-            temp = one
-            one = one + two
+            temp  =one
+            one = one +two
             two = temp
-            
         return one
